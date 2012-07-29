d3851c03738a77b1afdde1e2599765c2b770159b
Simon Ask Ulsnes <simon@ulsnes.dk>
2012-07-29 11:27:09 +0200

    Logging facilities.

--- log.cpp
+++ log.cpp
@@ -6,4 +6,42 @@
 //  Copyright (c) 2012 Simon Ask Consulting. All rights reserved.
 //
 
-#include <iostream>
+#include "base/log.hpp"
+#include <stdio.h>
+
+namespace falling {
+	struct LogManager::Impl {
+		Signal<const LogEntry&> on_log;
+		LogLevel current_log_level = DefaultLogLevel;
+	};
+	
+	LogManager::Impl& LogManager::get() {
+		static Impl* impl = new Impl;
+		return *impl;
+	}
+	
+	Signal<const LogEntry&>& LogManager::on_log() {
+		return get().on_log;
+	}
+	
+	LogLevel& LogManager::current_log_level() {
+		return get().current_log_level;
+	}
+	
+	void LogManager::log(const LogEntry& entry) {
+		if (entry.level <= current_log_level()) {
+			get().on_log(entry);
+			
+			if (get().on_log.num_connections() == 0) {
+				auto io = entry.level == LogLevelInformation ? stdout : stderr;
+				switch (entry.level) {
+					case LogLevelError: fprintf(io, "ERROR: "); break;
+					case LogLevelWarning: fprintf(io, "WARNING: "); break;
+					case LogLevelInformation: fprintf(io, "INFO: "); break;
+					case LogLevelDebug: fprintf(io, "DEBUG: "); break;
+				}
+				fprintf(io, "%s\n", entry.ss.str().c_str());
+			}
+		}
+	}
+}
--- log.hpp
+++ log.hpp
@@ -9,6 +9,91 @@
 #ifndef falling_log_hpp
 #define falling_log_hpp
 
+#include <sstream>
 
+#include "object/signal.hpp"
+
+namespace falling {
+	enum LogLevel {
+		LogLevelError,
+		LogLevelWarning,
+		LogLevelInformation,
+		LogLevelDebug,
+	};
+	
+#if defined(DEBUG)
+	static const LogLevel DefaultLogLevel = LogLevelDebug;
+#else
+	static const LogLevel DefaultLogLevel = LogLevelInformation;
+#endif
+	
+	struct LogEntry {
+		explicit LogEntry(LogLevel level) : level(level) {}
+		LogEntry(LogEntry&& other) : initialized(other.initialized), level(other.level), ss(std::move(other.ss)) {
+			other.initialized = false;
+		}
+		~LogEntry();
+		
+		template <typename T>
+		LogEntry& operator<<(T value) {
+			initialized = true;
+			ss << std::forward<T>(value);
+			return *this;
+		}
+		
+		bool initialized = false;
+		LogLevel level;
+		std::stringstream ss;
+	};
+	
+	
+	struct LogManager {
+		static void log(const LogEntry& entry);
+		static Signal<const LogEntry&>& on_log();
+		static LogLevel& current_log_level();
+	private:
+		struct Impl;
+		static Impl& get();
+	};
+	
+	inline LogEntry::~LogEntry() {
+		if (initialized) {
+			LogManager::log(*this);
+		}
+	}
+	
+	inline LogEntry Log(LogLevel level = LogLevelInformation) {
+		return LogEntry(level);
+	}
+	
+	inline LogEntry Error() {
+		return Log(LogLevelError);
+	}
+	
+	inline LogEntry Information() {
+		return Log(LogLevelInformation);
+	}
+	
+	inline LogEntry Warning() {
+		return Log(LogLevelWarning);
+	}
+	
+	struct DummyLogEntry {
+		template <typename T>
+		DummyLogEntry& operator<<(const T&) {
+			return *this;
+		}
+	};
+	
+#if defined(DEBUG)
+	inline LogEntry Debug() {
+		return LogEntry(LogLevelDebug);
+	}
+#else
+	inline DummyLogEntry Debug() {
+		return DummyLogEntry();
+	}
+#endif
+}
 
 #endif
