71ecb5c5abf728833ad3a41273a20f3e6f93b5f7
Simon Ask Ulsnes <simon@ulsnes.dk>
2012-07-29 23:36:06 +0200

    ResourcePtr type for resource system.

---
+++ resource_ptr_type.hpp
@@ -0,0 +1,57 @@
+//
+//  resource_ptr_type.hpp
+//  falling
+//
+//  Created by Simon Ask Ulsnes on 29/07/12.
+//  Copyright (c) 2012 Simon Ask Consulting. All rights reserved.
+//
+
+#ifndef falling_resource_ptr_type_hpp
+#define falling_resource_ptr_type_hpp
+
+#include "type/type.hpp"
+#include "io/resource_ptr.hpp"
+#include "io/resource_manager.hpp"
+
+namespace falling {
+	class ResourcePtrType : public Type {
+	public:
+		std::string name() const { return "ResourcePtr"; }
+	private:
+		std::string name_;
+	};
+	
+	template <typename T>
+	class ResourcePtrTypeImpl : public TypeFor<ResourcePtr<T>, ResourcePtrType> {
+	public:
+		virtual void deserialize(ResourcePtr<T>& place, const ArchiveNode& node, IUniverse&) const final;
+		virtual void serialize(const ResourcePtr<T>& place, ArchiveNode& node, IUniverse&) const final;
+	};
+	
+	template <typename T>
+	struct BuildTypeInfo<ResourcePtr<T>> {
+		static const ResourcePtrType* build() {
+			static const ResourcePtrTypeImpl<T>* type = new ResourcePtrTypeImpl<T>();
+			return type;
+		}
+	};
+	
+	template <typename T>
+	void ResourcePtrTypeImpl<T>::deserialize(ResourcePtr<T>& place, const ArchiveNode& node, IUniverse&) const {
+		ResourceID rid;
+		if (node.get(rid)) {
+			place = load_resource<T>(rid);
+		}
+	}
+	
+	template <typename T>
+	void ResourcePtrTypeImpl<T>::serialize(const ResourcePtr<T>& place, ArchiveNode& node, IUniverse&) const {
+		if (place != nullptr) {
+			node = place->resource_id();
+		} else {
+			node.clear();
+		}
+	}
+}
+
+#endif
