1af115b2dd203ca10cc2a143b646f98a4d1e3ba7
Simon Ask Ulsnes <simon@ulsnes.dk>
2012-07-29 11:26:03 +0200

    ArrayRef<T> class.

--- array_ref.hpp
+++ array_ref.hpp
@@ -9,6 +9,35 @@
 #ifndef falling_array_ref_hpp
 #define falling_array_ref_hpp
 
-
+namespace falling {
+	template <typename T>
+	struct ArrayRef {
+	public:
+		ArrayRef() : begin_(nullptr), end_(nullptr) {}
+		ArrayRef(T* begin, T* end) : begin_(begin), end_(end) { ASSERT(begin_ <= end_); }
+		ArrayRef(const ArrayRef<T>& other) = default;
+		ArrayRef(ArrayRef<T>&& other) = default;
+		ArrayRef<T>& operator=(const ArrayRef<T>& other) = default;
+		ArrayRef<T>& operator=(ArrayRef<T>&& other) = default;
+		bool operator==(const ArrayRef<T>& other) const { return begin_ == other.begin_ && end_ == other.end_; }
+		bool operator!=(const ArrayRef<T>& other) const { return begin_ != other.begin_ && end_ != other.end_; }
+		
+		size_t size() const { return end_ - begin_; }
+		
+		T& operator[](size_t idx) { ASSERT(idx < size()); return begin_[idx]; }
+		const T& operator[](size_t idx) const { ASSERT(idx < size()); return begin_[idx]; }
+		
+		typedef T value_type;
+		typedef T* iterator;
+		typedef const T* const_iterator;
+		iterator begin() { return begin_; }
+		iterator end() { return end_; }
+		const_iterator begin() const { return begin_; }
+		const_iterator end() const { return end_; }
+	private:
+		T* begin_;
+		T* end_;
+	};
+}
 
 #endif
