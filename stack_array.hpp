48792a6be59d4c873cad773b7d4b4403ec604bdb
Simon Ask Ulsnes <simon@ulsnes.dk>
2012-07-29 11:30:26 +0200

    StackArray<T> implementation (alloca wrapper).

--- stack_array.hpp
+++ stack_array.hpp
@@ -9,6 +9,26 @@
 #ifndef falling_stack_array_hpp
 #define falling_stack_array_hpp
 
+#include "base/array_ref.hpp"
 
+namespace falling {
+	template <typename T>
+	struct StackArrayHolder {
+		T* begin;
+		T* end;
+		StackArrayHolder(T* begin, T* end) : begin(begin), end(end) {
+			construct_range(begin, end);
+		}
+		~StackArrayHolder() {
+			destruct_range(begin, end);
+		}
+	};
+	
+#define DEFINE_STACK_ARRAY(T, NAME, SIZE) \
+	T* NAME ## _data_ ## __LINE__ = (T*)alloca(sizeof(T) * SIZE); \
+	StackArrayHolder<T> NAME ## _holder_ ## __LINE__(NAME ## _data_ ## __LINE__, NAME ## _data_ ## __LINE__ + SIZE); \
+	ArrayRef<T> NAME(NAME ## _data_ ## __LINE__, NAME ## _data_ ## __LINE__ + SIZE)
+
+}
 
 #endif
