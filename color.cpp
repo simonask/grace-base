bd5717521ccb1d2601e24ef296766941339854d8
Simon Ask Ulsnes <simon@ulsnes.dk>
2012-07-29 11:28:22 +0200

    Color class.

--- color.cpp
+++ color.cpp
@@ -6,4 +6,17 @@
 //  Copyright (c) 2012 Simon Ask Consulting. All rights reserved.
 //
 
-#include <iostream>
+#include "base/color.hpp"
+
+namespace falling {
+#define DEF_COLOR(NAME, R, G, B) const Color Color::NAME = Color(R, G, B, 1.f)
+	DEF_COLOR(White, 1.f, 1.f, 1.f);
+	DEF_COLOR(Black, 0, 0, 0);
+	DEF_COLOR(Red, 1, 0, 0);
+	DEF_COLOR(Green, 0, 1, 0);
+	DEF_COLOR(Blue, 0, 0, 1);
+	DEF_COLOR(Pink, 1.f, 0.f, 1.f);
+	DEF_COLOR(Cyan, 0, 1, 1);
+	DEF_COLOR(Yellow, 1, 1, 0);
+	const Color Color::Transparent = Color(0, 0, 0, 0);
+}
--- color.hpp
+++ color.hpp
@@ -9,6 +9,77 @@
 #ifndef falling_color_hpp
 #define falling_color_hpp
 
+#include "base/vector.hpp"
+#include <initializer_list>
 
+namespace falling {
+	struct Color {
+	public:
+		typedef vec4 ColorComponents;
+		typedef ColorComponents::ComponentType ComponentType;
+		
+		Color() {}
+		constexpr Color(ComponentType r, ComponentType g, ComponentType b, ComponentType a = 1.0) : components_(r, g, b, a) {}
+		Color(const Color& other) = default;
+		explicit Color(ColorComponents components) : components_(components) {}
+		Color& operator=(const Color&) = default;
+		ColorComponents components() const { return components_; }
+		
+		ComponentType  red() const   { return components_[0]; }
+		ComponentType& red()         { return components_[0]; }
+		ComponentType  green() const { return components_[1]; }
+		ComponentType& green()       { return components_[1]; }
+		ComponentType  blue() const  { return components_[2]; }
+		ComponentType& blue()        { return components_[2]; }
+		ComponentType  alpha() const { return components_[3]; }
+		ComponentType& alpha()       { return components_[3]; }
+		
+		template <typename Function>
+		auto pass_as_rgb(Function function) const
+		-> decltype(function(ComponentType(), ComponentType(), ComponentType()))
+		{
+			return function(red(), green(), blue());
+		}
+		
+		template <typename Function>
+		auto pass_as_rgba(Function function) const
+		-> decltype(function(ComponentType(), ComponentType(), ComponentType(), ComponentType()))
+		{
+			return function(red(), green(), blue(), alpha());
+		}
+		
+		template <typename Function>
+		auto pass_as_bgr(Function function) const
+		-> decltype(function(ComponentType(), ComponentType(), ComponentType()))
+		{
+			return function(blue(), green(), red());
+		}
+		
+		template <typename Function>
+		auto pass_as_bgra(Function function) const
+		-> decltype(function(ComponentType(), ComponentType(), ComponentType(), ComponentType()))
+		{
+			return function(blue(), green(), red(), alpha());
+		}
+		
+		static const Color White;
+		static const Color Black;
+		static const Color Red;
+		static const Color Green;
+		static const Color Blue;
+		static const Color Pink;
+		static const Color Cyan;
+		static const Color Yellow;
+		static const Color Transparent;
+	private:
+		ColorComponents components_;
+	};
+	
+	inline Color interpolate(Color a, Color b, Color::ComponentType weight = 0.5) {
+		auto diff = b.components() - a.components();
+		auto weighted_diff = diff * Color::ColorComponents::replicate(weight); 
+		return Color(a.components() + weighted_diff);
+	}
+}
 
 #endif
