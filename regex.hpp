30cbde3e03efd8e7e25760e455ced1ebd6aa7923
Simon Ask Ulsnes <simon@ulsnes.dk>
2012-08-02 14:23:06 +0200

    Regex class wrapping std::regex.

--- regex.hpp
+++ regex.hpp
@@ -9,6 +9,82 @@
 #ifndef falling_regex_hpp
 #define falling_regex_hpp
 
+#include <regex>
+#include <string>
 
+#include "base/basic.hpp"
+
+namespace falling {
+	struct Regex;
+}
+
+namespace std {
+	void swap(falling::Regex&, falling::Regex&);
+}
+
+namespace falling {
+	/*
+	 This class is mostly necessary because std::regex doesn't save a
+	 string representation of the regex, so it can't be printed.
+	 */
+	struct Regex {
+		Regex(std::string definition, std::regex::flag_type flags = std::regex_constants::ECMAScript);
+		Regex(Regex&&) = default;
+		Regex(const Regex&) = default;
+		Regex& operator=(Regex&&) = default;
+		Regex& operator=(const Regex&) = default;
+		
+		operator const std::string&() const { return definition; }
+		operator const std::regex&() const { return regex; }
+		
+		FORWARD_TO_MEMBER(mark_count, regex, std::regex)
+		FORWARD_TO_MEMBER(flags, regex, std::regex)
+		FORWARD_TO_MEMBER(getloc, regex, std::regex)
+		
+		template <typename BidirectionalIterator>
+		bool match(BidirectionalIterator begin, BidirectionalIterator end) const;
+		template <typename BidirectionalIterator>
+		bool match(BidirectionalIterator begin, BidirectionalIterator end, std::match_results<BidirectionalIterator>& out_results) const;
+		template <typename BidirectionalIterator>
+		bool search(BidirectionalIterator begin, BidirectionalIterator end) const;
+		
+		// TODO:
+		template <typename OutputIterator, typename BidirectionalIterator>
+		OutputIterator replace(OutputIterator out, BidirectionalIterator begin, BidirectionalIterator end);
+	private:
+		std::string definition;
+		std::regex regex;
+		friend void std::swap(Regex&, Regex&);
+	};
+	
+	inline Regex::Regex(std::string definition, std::regex::flag_type flags)
+	: definition(std::move(definition))
+	, regex(this->definition.c_str(), this->definition.size(), flags)
+	{}
+	
+	template <typename BidirectionalIterator>
+	bool Regex::match(BidirectionalIterator begin, BidirectionalIterator end) const {
+		return std::regex_match(begin, end, regex);
+	}
+	
+	template <typename BidirectionalIterator>
+	bool Regex::match(BidirectionalIterator begin, BidirectionalIterator end, std::match_results<BidirectionalIterator>& out_results) const {
+		return std::regex_match(begin, end, out_results, regex);
+	}
+	
+	template <typename BidirectionalIterator>
+	bool Regex::search(BidirectionalIterator begin, BidirectionalIterator end) const {
+		return std::regex_search(begin, end, regex);
+	}
+	
+	
+}
+
+namespace std {
+	inline void swap(falling::Regex& lhs, falling::Regex& rhs) {
+		std::swap(lhs.definition, rhs.definition);
+		std::swap(lhs.regex, rhs.regex);
+	}
+}
 
 #endif
