c39ae6b20ffc352c1ed72790232773ff75730bdf
Simon Ask Ulsnes <simon@ulsnes.dk>
2012-11-02 22:44:13 +0000

    make_rotation_matrix33_z, make_2d_translation_matrix33

--- geometry.hpp
+++ geometry.hpp
@@ -48,6 +48,29 @@ namespace falling {
 		vec2 r2 = shuffle2<0, X, 1, Y>(msinv, cosv);
 		return matrix22::from_rows({r1, r2});
 	}
+	
+	// TODO: Arbitrary rotation matrices for 3D-space
+	inline matrix33 make_rotation_matrix33_z(float32 theta) {
+		vec3 v = vec3::replicate(theta);
+		vec3 cosv = cos(v);
+		vec3 sinv = sin(v);
+		vec3 msinv = -sinv;
+		vec3 r1 = shuffle2<0, X, 1, Y, 0, IgnoreAxis>(cosv, sinv);
+		vec3 r2 = shuffle2<0, X, 1, Y, 0, IgnoreAxis>(msinv, cosv);
+		vec3 r3 = vec3::zero();
+		r1.z = 0.f;
+		r2.z = 0.f;
+		r3.z = 1.f; // identity
+		return matrix33::from_rows({r1, r2, r3});
+	}
+	
+	inline matrix33 make_2d_translation_matrix33(vec2 translation) {
+		matrix33 trans = matrix33::identity();
+		trans.row_at(0).z = translation.x;
+		trans.row_at(1).z = translation.y;
+		ASSERT(trans.row_at(2).z == 1.f);
+		return trans;
+	}
 }
 
 #endif
