88bcb740861ccedce8b777485ec81630da9105e8
Simon Ask Ulsnes <simon@ulsnes.dk>
2012-11-24 15:20:44 +0000

    Semi-usable ArrayList implementation, now using IAllocator.

--- array_list.hpp
+++ array_list.hpp
@@ -10,235 +10,231 @@
 #define falling_array_list_hpp
 
 #include "base/basic.hpp"
+#include "memory/allocator.hpp"
+#include "base/intrusive_list.hpp"
 
 namespace falling {
-	namespace detail {
-		struct ArrayListPage {
-			void* operator new(size_t sz);
-			void operator delete(void* ptr);
-		private:
-			void* operator new[](size_t);
-			void operator delete[](void*);
-		};
-	}
-	
-	
 	template <typename T>
-	class ArrayList {
-	public:
-		ArrayList() : head_(nullptr), tail_(nullptr) {}
-		ArrayList(const ArrayList<T>& other);
-		ArrayList(ArrayList<T>&& other);
-		~ArrayList() { clear(); }
-		ArrayList<T>& operator=(const ArrayList<T>& other);
-		ArrayList<T>& operator=(ArrayList<T>&& other);
-		
-		template <bool IsConst>
-		struct Iterator;		
-		typedef T value_type;
-		typedef Iterator<false> iterator;
-		typedef Iterator<true> const_iterator;
-		
-		T& operator[](uint32 idx);
-		const T& operator[](uint32 idx) const;
-		
-		uint32 size() const;
-		uint32 capacity() const;
-		void push_back(T element);
-		void resize(uint32, T fill = T());
-		void reserve(uint32);
-		void clear();
-		
-		template <typename InputIterator>
-		void insert(InputIterator begin, InputIterator end);
-		template <typename InputIterator>
-		void insert(InputIterator begin, InputIterator end, iterator position);
-		void insert(T element, iterator position);
-		
-		template <typename... Args>
-		void emplace_back(Args... args);
-		
-		iterator begin();
-		iterator end();
-		const_iterator begin();
-		const_iterator end();
-		
-		iterator erase(iterator);
-		iterator at(uint32 idx);
-		
-	private:
-		struct Page : detail::ArrayListPage {
-			typedef std::aligned_storage<sizeof(T), std::alignment_of<T>::value>::type Element;
-			
-			Page* previous;
-			Page* next;
-			Element* begin;
-			Element* current;
-			Element* end;
-			
-			size_t available() const { return end - current; }
-		};
-		
-		static const PageSize = 4096;
-		static const PageHeaderSize = round_up(sizeof(Page), sizeof(T));
-		static const DataPerPage = PageSize - PageHeaderSize;
-		static const ElementsPerPage = DataPerPage / sizeof(T);
-		
-		Page* head_;
-		Page* tail_;
-		size_t size_;
-		size_t num_pages_;
-		
-		Page* create_page();
-	};
-	
-	template <typename T>
-	ArrayList<T>::ArrayList(const ArrayList<T>& other) : head_(nullptr), tail_(nullptr), size_(0), num_pages_(0) {
-		insert(other.begin(), other.end());
-	}
-	
-	template <typename T>
-	ArrayList<T>::ArrayList(ArrayList<T>&& other) {
-		head_ = other.head_;
-		tail_ = other.tail_;
-		size_ = other.size_;
-		num_pages_ = other.num_pages_;
-		other.head_ = nullptr;
-		other.tail_ = nullptr;
-		other.size_ = 0;
-		other.num_pages_ = 0;
-	}
-	
-	template <typename T>
-	ArrayList<T>& ArrayList<T>::operator=(const ArrayList<T>& other) {
-		clear();
-		insert(other.begin(), other.end());
-		return *this;
-	}
-	
-	template <typename T>
-	ArrayList<T>& ArrayList<T>::operator=(ArrayList<T>&& other) {
-		clear();
-		head_ = other.head_;
-		tail_ = other.tail_;
-		size_ = other.size_;
-		num_pages_ = other.num_pages_;
-		other.head_ = nullptr;
-		other.tail_ = nullptr;
-		other.size_ = 0;
-		other.num_pages_ = 0;
-		return *this;
-	}
-	
-	template <typename T>
-	T& ArrayList<T>::operator[](uint32 idx) {
-		for (Page* p = head_; p != nullptr; p = p->next) {
-			if (idx < ElementsPerPage) {
-				ASSERT(idx < (p->current - p->begin)); // out of bounds!
-				return p->begin[idx];
-			}
-			idx -= ElementsPerPage;
-		}
-		ASSERT(false); // out of bounds!
-	}
-	
-	template <typename T>
-	const T& ArrayList<T>::operator[](uint32 idx) const {
-		for (Page* p = head_; p != nullptr; p = p->next) {
-			if (idx < ElementsPerPage) {
-				ASSERT(idx < (p->current - p->begin)); // out of bounds!
-				return p->begin[idx];
-			}
-			idx -= ElementsPerPage;
-		}
-		ASSERT(false); // out of bounds!
-	}
-	
-	template <typename T>
-	uint32 ArrayList<T>::size() const {
-		return size_;
-	}
-	
-	template <typename T>
-	uint32 ArrayList<T>::capacity() const {
-		return num_pages_ * ElementsPerPage;
-	}
-	
-	template <typename T>
-	void ArrayList<T>::push_back(T element) {
-		insert(std::move(element), end());
-	}
-	
-	template <typename T>
-	void ArrayList<T>::resize(uint32 new_size, T element) {
-		size_t sz = size();
-		if (new_size < sz) {
-			auto i0 = at(new_size);
-			auto i1 = end() - 1;
-			for (auto it = i1; it != i0-1; --it) {
-				erase(it);
-			}
-		} else if (new_size > sz) {
-			size_t diff = new_size - sz;
-			reserve(new_size);
-			for (size_t i = 0; i < diff; ++i) {
-				push_back(element);
-			}
-		}
-	}
-	
-	template <typename T>
-	void ArrayList<T>::reserve(uint32 new_capacity) {
-		while (capacity() < new_capacity) {
-			Page* p = create_page();
-			if (tail_ == nullptr) {
-				head_ = tail_ = p;
-			} else {
-				tail_->next = p;
-				p->previous = tail_;
-				tail_ = p;
-			}
-		}
-	}
-	
-	template <typename T>
-	void ArrayList<T>::clear() {
-		for (Page* p = head_; p != nullptr;) {
-			for (Page::Element* e = p->begin; e != p->current; ++e) {
-				T* x = reinterpret_cast<T*>(e);
-				x->~T();
-			}
-			
-			Page* next = p->next;
-			delete p;
-			p = next;
-		}
-		size_ = 0;
-		num_pages_ = 0;
-	}
-	
-	template <typename T>
-	template <typename InputIterator>
-	void ArrayList<T>::insert(InputIterator i0, InputIterator i1) {
-		insert(std::forward<InputIterator>(i0), std::forward<InputIterator>(i1), end());
-	}
-	
-	template <typename T>
-	template <typename InputIterator>
-	void ArrayList<T>::insert(InputIterator i0, InputIterator i1, iterator position) {
-		size_t enlarge_by = i1 - i0;
-		reserve(size() + enlarge_by);
-		
-		// shift tail to the right
-		auto shr_i0 = position;
-		auto shr_i1 = end();
-		size_t shr_n = shr_i1 - shr_i0;
-		size_t shifted = 0;
-		Page* shr_p = tail_;
-		while (shifted < shr_n) {
-			
-		}
-	}
+    class ArrayList {
+    public:
+        explicit ArrayList(IAllocator& allocator = default_allocator());
+        ArrayList(IAllocator& allocator, const ArrayList<T>& other);
+        ArrayList(const ArrayList& other);
+        ArrayList(ArrayList&& other);
+        ~ArrayList();
+        
+        IAllocator& allocator() const;
+        
+        size_t size() const;
+        
+        void push_back(const T& x);
+        void push_back(T&& x);
+        void clear();
+        T& front();
+        const T& front() const;
+        T& back();
+        const T& back() const;
+        
+        T& operator[](size_t idx);
+        const T& operator[](size_t idx) const;
+        
+        template <bool IsConst> struct iterator_impl;
+        using iterator = iterator_impl<false>;
+        using const_iterator = iterator_impl<true>;
+        
+        iterator begin();
+        iterator end();
+        const_iterator begin() const;
+        const_iterator end() const;
+        
+        template <typename InputIterator>
+        void insert(InputIterator i0, InputIterator i1);
+        template <typename InputIterator>
+        void insert(InputIterator i0, InputIterator i1, iterator before);
+        iterator erase(iterator it);
+    private:
+        template <bool> friend struct iterator_impl;
+        
+        IAllocator& allocator_;
+        
+        struct Block {
+            Block* next;
+            T* begin;
+            T* end;
+            T* current;
+        };
+        
+        Block* head_ = nullptr;
+        Block* tail_ = nullptr;
+        size_t size_ = 0;
+        
+        Block* create_and_append_block();
+    };
+    
+    template <typename T>
+    ArrayList<T>::ArrayList(IAllocator& alloc) : allocator_(alloc) {}
+    
+    template <typename T>
+    ArrayList<T>::~ArrayList() {
+        clear();
+    }
+    
+    template <typename T>
+    IAllocator& ArrayList<T>::allocator() const {
+        return allocator_;
+    }
+    
+    template <typename T>
+    size_t ArrayList<T>::size() const {
+        return size_;
+    }
+    
+    template <typename T>
+    void ArrayList<T>::push_back(const T& object) {
+        Block* b = tail_;
+        if (b == nullptr || b->end - b->current == 0) {
+            b = create_and_append_block();
+        }
+        
+        ASSERT(b->end - b->current > 0);
+        T* ptr = b->current++;
+        ++size_;
+        new(ptr) T(object);
+    }
+    
+    template <typename T>
+    void ArrayList<T>::clear() {
+        Block* b = head_;
+        while (b) {
+            Block* tmp = b->next;
+            for (T* p = b->begin; p < b->current; ++p) {
+                p->~T();
+            }
+            size_t block_size = (byte*)b->end - (byte*)b;
+            allocator_.free_large(b, block_size);
+            b = tmp;
+        }
+        head_ = nullptr;
+        tail_ = nullptr;
+    }
+    
+    template <typename T>
+    typename ArrayList<T>::Block* ArrayList<T>::create_and_append_block() {
+        size_t actual_allocation_size;
+        byte* memory = (byte*)allocator_.allocate_large(4096, alignof(Block), actual_allocation_size);
+        Block* b = new(memory) Block;
+        
+        byte* begin = memory + sizeof(Block);
+        intptr_t ibegin = reinterpret_cast<intptr_t>(begin);
+        intptr_t adjust = (ibegin % alignof(T)) & (alignof(T)-1);
+        begin += adjust;
+        b->begin = reinterpret_cast<T*>(begin);
+        b->end = reinterpret_cast<T*>(memory + actual_allocation_size);
+        b->current = b->begin;
+        b->next = nullptr;
+        
+        if (head_ == nullptr) {
+            head_ = b;
+        }
+        if (tail_ != nullptr) {
+            tail_->next = b;
+        }
+        tail_ = b;
+        
+        return b;
+    }
+    
+    template <typename T>
+    template <bool IsConst>
+    struct ArrayList<T>::iterator_impl {
+    public:
+        using Self = iterator_impl<IsConst>;
+        using Owner = typename std::conditional<IsConst, const ArrayList<T>, ArrayList<T>>::type;
+        using ValueType = typename std::conditional<IsConst, const T, T>::type;
+        using value_type = ValueType;
+        
+        ValueType& operator*() const {
+            return *current_;
+        }
+        
+        ValueType* operator->() const {
+            return current_;
+        }
+        
+        Self& operator++() {
+            ++current_;
+            if (current_ >= block_->current) {
+                block_ = block_->next;
+                if (block_) {
+                    current_ = block_->begin;
+                } else {
+                    current_ = nullptr;
+                }
+            }
+            return *this;
+        }
+        
+        Self operator++(int) {
+            Self s = *this;
+            ++s;
+            return s;
+        }
+        
+        // TODO: Reverse iterators?
+        
+        bool operator==(const iterator_impl<true>& other) const {
+            return owner_ == other.owner_ && block_ == other.block_ && current_ == other.current_;
+        }
+        bool operator!=(const iterator_impl<true>& other) const {
+            return !(*this == other);
+        }
+        
+        iterator_impl(const iterator_impl<false>& other) : owner_(other.owner_), block_(other.block_), current_(other.current_) {}
+        template <bool IsConst_ = IsConst>
+        iterator_impl(const typename std::enable_if<IsConst_, iterator_impl<true>>::type& other) : owner_(other.owner_), block_(other.block_), current_(other.current_) {}
+        Self& operator=(const iterator_impl<false>& other) {
+            owner_ = other.owner_;
+            block_ = other.block_;
+            current_ = other.current_;
+            return *this;
+        }
+        template <bool IsConst_ = IsConst>
+        Self& operator=(const typename std::enable_if<IsConst_, iterator_impl<true>>::type& other) {
+            owner_ = other.owner_;
+            block_ = other.block_;
+            current_ = other.current_;
+            return *this;
+        }
+    private:
+        iterator_impl(Owner& owner, Block* b, ValueType* c) : owner_(&owner), block_(b), current_(c) {}
+        
+        friend class ArrayList<T>;
+        friend struct iterator_impl<!IsConst>;
+        using Block = typename Owner::Block;
+        Owner* owner_;
+        Block* block_;
+        ValueType* current_;
+    };
+    
+    template <typename T>
+    typename ArrayList<T>::iterator ArrayList<T>::begin() {
+        return iterator(*this, head_, head_ ? head_->begin : nullptr);
+    }
+    
+    template <typename T>
+    typename ArrayList<T>::iterator ArrayList<T>::end() {
+        return iterator(*this, nullptr, nullptr);
+    }
+    
+    template <typename T>
+    typename ArrayList<T>::const_iterator ArrayList<T>::begin() const {
+        return const_iterator(*this, head_, head_ ? head_->begin : nullptr);
+    }
+    
+    template <typename T>
+    typename ArrayList<T>::const_iterator ArrayList<T>::end() const {
+        return const_iterator(*this, nullptr, nullptr);
+    }
 }
 
 #endif
