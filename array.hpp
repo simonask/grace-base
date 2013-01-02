4357e2eb4a5024d62ab4a988437c3f9d1eb48db9
Simon Ask Ulsnes <simon@ulsnes.dk>
2013-01-02 15:22:22 +0100

    array

--- array.hpp
+++ array.hpp
@@ -46,10 +46,6 @@ public:
 		return ref();
 	}
 	
-	operator ArrayRef<const T>() const {
-		return ArrayRef<const T>(data_, data_ + size_);
-	}
-	
 	T& operator[](size_t idx);
 	const T& operator[](size_t idx) const;
 	
