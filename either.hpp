9b7655e62a81b5e8b4a8ca62087b5df266f78006
Simon Ask Ulsnes <simon@ulsnes.dk>
2013-05-05 15:31:56 +0100

    * Moved type traits to type_traits.hpp instead of either.hpp. * Added All-/AnyTypesSatisfyTrait templates. * Either: Tightened requirements, so only constructor/assignment routines supported by all types will be supported.

--- either.hpp
+++ either.hpp
@@ -11,60 +11,69 @@
 
 #include "base/basic.hpp"
 #include "base/type_info.hpp"
+#include "base/type_traits.hpp"
 
 namespace falling {
-	template <typename A, typename B>
-	constexpr auto max(A a, B b) -> typename std::common_type<A, B>::type {
-		return a > b ? a : b;
-	}
-
-	template <typename... Types> struct MaxSize;
-	template <> struct MaxSize<> {
-		static const size_t Value = 0;
-	};
-	template <typename T, typename... Rest> struct MaxSize<T, Rest...> {
-		static const size_t Value = max(sizeof(T), MaxSize<Rest...>::Value);
-	};
-	template <typename... Types> struct MaxAlignment;
-	template <> struct MaxAlignment<> {
-		static const size_t Value = 0;
-	};
-	template <typename T, typename... Rest> struct MaxAlignment<T, Rest...> {
-		static const size_t Value = max(alignof(T), MaxAlignment<Rest...>::Value);
-	};
-	
-	template <typename... Types>
-	struct AlignedUnion {
-		using Type = typename std::aligned_storage<MaxSize<Types...>::Value, MaxAlignment<Types...>::Value>::type;
-	};
-	
-	template <typename T, size_t I, typename... Types> struct IndexOfTypeImpl;
-	template <typename T, size_t I, typename Current, typename... Rest> struct IndexOfTypeImpl<T, I, Current, Rest...> {
-		static const size_t Value = std::is_same<T, Current>::value ? I : IndexOfTypeImpl<T, I+1, Rest...>::Value;
-	};
-	template <typename T, size_t I> struct IndexOfTypeImpl<T, I> {
-		static const size_t Value = SIZE_MAX;
-	};
-	template <typename T, typename... Types> struct IndexOfType {
-		static const size_t Value = IndexOfTypeImpl<T, 0, Types...>::Value;
-	};
-	
 	template <typename... Types> // max 255 types!
 	class Either {
 	public:
 		static_assert(sizeof...(Types) < UINT8_MAX, "Too many possible types for Either.");
 	
 		using Self = Either<Types...>;
+		static const bool IsCopyConstructible = AreAllCopyConstructible<Types...>::Value;
+		static const bool IsCopyAssignable = AreallCopyAssignable<Types...>::Value;
+		static const bool IsMoveConstructible = AreAllMoveConstructible<Types...>::Value;
+		static const bool IsMoveAssignable = AreallMoveAssignable<Types...>::Value;
 		
-		template <typename T>
-		explicit Either(T value);
-		Either(const Self& other);
-		Either(Self&& other);
+		template <typename T, typename = typename std::enable_if<IndexOfType<T, Types...>::Value < sizeof...(Types)>::type>
+		explicit Either(const T& value) {
+			static const auto I = IndexOfType<T, Types...>::Value;
+			static_assert(I < sizeof...(Types), "T is not a valid type for this Either.");
+			construct_copy(value);
+		}
+		
+		template <typename T, typename = typename std::enable_if<IndexOfType<T, Types...>::Value < sizeof...(Types)>::type>
+		explicit Either(T&& value) {
+			static const auto I = IndexOfType<T, Types...>::Value;
+			static_assert(I < sizeof...(Types), "T is not a valid type for this Either.");
+			construct_move(move(value));
+		}
+		
+		
+		Either(const Self& other) {
+			static_assert(IsCopyConstructible, "Cannot copy-construct this Either, because one of the inner types does not support it.");
+			construct_copy(other);
+		}
+		Either(Self&& other) {
+			static_assert(IsMoveConstructible, "Cannot move-construct this Either, because one of the inner types does not support it.");
+			construct_move(move(other));
+		}
+		
 		~Either();
-		template <typename T>
-		Self& operator=(T value);
-		Self& operator=(const Self& other);
-		Self& operator=(Self&& other);
+		
+		template <typename T, typename = typename std::enable_if<IsMoveAssignable && IndexOfType<T, Types...>::Value < sizeof...(Types)>::type>
+		Self& operator=(T&& value) {
+			assign_move<T>(move(value));
+			return *this;
+		}
+		
+		template <typename T, typename = typename std::enable_if<IsCopyAssignable && IndexOfType<T, Types...>::Value < sizeof...(Types)>::type>
+		Self& operator=(const T& value) {
+			assign_copy<T>(value);
+			return *this;
+		}
+		
+		Self& operator=(const Self& other) {
+			static_assert(IsCopyAssignable, "Cannot copy-assign this Either, because one of the inner types does not support it.");
+			assign_copy(other);
+			return *this;
+		}
+		
+		Self& operator=(Self&& other) {
+			static_assert(IsMoveAssignable, "Cannot move-assign this Either, because one of the inner types does not support it.");
+			assign_move(move(other));
+			return *this;
+		}
 		
 		template <typename T>
 		typename std::enable_if<IndexOfType<T, Types...>::Value < sizeof...(Types), bool>::type
@@ -124,6 +133,7 @@ namespace falling {
 		template <typename Visitor>
 		void visit(Visitor& visitor) const;
 		
+		template <typename = typename std::enable_if<(IsMoveAssignable && IsMoveConstructible) || (IsCopyAssignable && IsCopyConstructible)>::type>
 		void swap(Self& other);
 		
 		const TypeInfo& type_info() const {
@@ -133,10 +143,13 @@ namespace falling {
 		byte* memory() { return reinterpret_cast<byte*>(&memory_); }
 		const byte* memory() const { return reinterpret_cast<const byte*>(&memory_); }
 	private:
+		// Unconstructed contents -- must be constructed with construct_move/construct_copy before destructor is called!
+		Either() {}
+	
 		template <bool> friend struct WhenControlFlow;
 		using Storage = typename AlignedUnion<Types...>::Type;
 		Storage memory_;
-		uint8 type_index_;
+		uint8 type_index_ = UINT8_MAX;
 		static constexpr const TypeInfo* const type_infos_[sizeof...(Types)] = {&GetTypeInfo<Types>::Value...};
 		
 		void assign_move(Self&& other);
@@ -145,112 +158,140 @@ namespace falling {
 		void assign_move(T&& other);
 		template <typename T>
 		void assign_copy(const T& other);
+		void construct_move(Self&& other);
+		void construct_copy(const Self& other);
+		template <typename T>
+		void construct_move(T&& other);
+		template <typename T>
+		void construct_copy(const T& other);
+		void destruct();
 	};
 	
 	template <typename... Types>
 	constexpr const TypeInfo* const Either<Types...>::type_infos_[sizeof...(Types)];
 	
 	template <typename... Types>
-	template <typename T>
-	Either<Types...>::Either(T value) {
-		static const auto I = IndexOfType<T, Types...>::Value;
-		static_assert(I < sizeof...(Types), "T is not a valid type for this Either.");
-		type_index_ = I;
-		type_info().move_or_copy_construct((byte*)&memory_, (byte*)&value);
-	}
-	
-	template <typename... Types>
-	Either<Types...>::Either(const Self& other) {
-		assign_copy(other);
-	}
-	
-	template <typename... Types>
-	Either<Types...>::Either(Self&& other) {
-		assign_move(move(other));
-	}
-	
-	template <typename... Types>
 	Either<Types...>::~Either() {
-		type_info().destruct((byte*)&memory_);
+		destruct();
 	}
 	
 	template <typename... Types>
-	template <typename T>
-	Either<Types...>& Either<Types...>::operator=(T value) {
-		assign_move(move(value));
-		return *this;
-	}
-	
-	template <typename... Types>
-	Either<Types...>& Either<Types...>::operator=(const Self& other) {
-		assign_copy(other);
-		return *this;
-	}
-	
-	template <typename... Types>
-	Either<Types...>& Either<Types...>::operator=(Self&& other) {
-		assign_move(move(other));
-		return *this;
-	}
-	
-	template <typename... Types>
+	template <typename> // enable_if IsMoveAssignable || IsCopyAssignable
 	void Either<Types...>::swap(Self& other) {
-		Storage tmp_;
+		Self tmp; // Uninitialized.
 		if (is_same_type_as(other)) {
-			type_info().move_or_copy_construct((byte*)&tmp_, (byte*)&other.memory_);
-			type_info().move_or_copy_assing((byte*)&other.memory_, (byte*)&memory_);
-			type_info().move_or_copy_assign((byte*)memory_, (byte*)&tmp_);
-			type_info().destruct((byte*)&tmp_);
+			if (IsMoveConstructible && IsMoveAssignable) {
+				tmp.construct_move(move(other));
+				other.assign_move(move(*this));
+				assign_move(tmp);
+			} else if (IsCopyConstructible && IsMoveConstructible) {
+				tmp.construct_copy(other);
+				other.assign_copy(*this);
+				assign_copy(tmp);
+			} else {
+				UNREACHABLE();
+			}
 		} else {
-			other.type_info().move_or_copy_construct((byte*)&tmp_, (byte*)&other.memory_);
-			other.type_info().destruct((byte*)&other.memory_);
-			type_info().move_or_copy_construct((byte*)&other.memory_, (byte*)&memory_);
-			type_info().destruct((byte*)&memory_);
-			other.type_info().move_or_copy_construct((byte*)&memory_, (byte*)&tmp_);
-			std::swap(type_index_, other.type_index_);
+			if (IsMoveConstructible) {
+				tmp.construct_move(move(other));
+				other.destruct();
+				other.construct_move(move(*this));
+				destruct();
+				construct_move(move(tmp));
+			} else if (IsCopyConstructible) {
+				tmp.construct_copy(other);
+				other.destruct();
+				other.construct_copy(*this);
+				destruct();
+				construct_copy(tmp);
+			} else {
+				UNREACHABLE();
+			}
 		}
 	}
 	
 	template <typename... Types>
+	void Either<Types...>::destruct() {
+		ASSERT(type_index_ < sizeof...(Types));
+		type_info().destruct(memory());
+		detail::poison_memory(memory(), memory() + sizeof(memory_), detail::FREED_MEMORY_PATTERN);
+		type_index_ = UINT8_MAX;
+	}
+	
+	template <typename... Types>
 	void Either<Types...>::assign_move(Self&& other) {
-		swap(other);
+		if (is_same_type_as(other)) {
+			type_info().move_assign(memory(), other.memory());
+		} else {
+			destruct();
+			construct_move(other);
+		}
 	}
 	
 	template <typename... Types>
 	void Either<Types...>::assign_copy(const Self &other) {
 		if (is_same_type_as(other)) {
-			type_info().copy_assign((byte*)&memory_, (const byte*)&other.memory_);
+			type_info().copy_assign(memory(), other.memory());
 		} else {
-			type_info().destruct((byte*)&memory_);
-			type_index_ = other.type_index_;
-			type_info().copy_construct((byte*)&memory_, (const byte*)&other.memory_);
+			destruct();
+			construct_copy(other);
 		}
+	}
+	
+	template <typename... Types>
+	void Either<Types...>::construct_move(Self&& other) {
+		ASSERT(type_index_ == UINT8_MAX); // Already constructed!
+		type_index_ = other.type_index_;
+		detail::poison_memory(memory(), memory() + sizeof(memory_), detail::UNINITIALIZED_MEMORY_PATTERN);
+		type_info().move_construct(memory(), other.memory());
+	}
+	
+	template <typename... Types>
+	void Either<Types...>::construct_copy(const Self &other) {
+		ASSERT(type_index_ == UINT8_MAX); // Already constructed!
+		type_index_ = other.type_index_;
+		detail::poison_memory(memory(), memory() + sizeof(memory_), detail::UNINITIALIZED_MEMORY_PATTERN);
+		type_info().copy_construct(memory(), other.memory());
 	}
 	
 	template <typename... Types>
 	template <typename T>
 	void Either<Types...>::assign_move(T&& value) {
-		static const size_t I = IndexOfType<T, Types...>::Value;
 		if (this->is_a<T>()) {
-			type_info().move_or_copy_assign((byte*)&memory_, (byte*)&value);
+			*reinterpret_cast<T*>(memory()) = move(value);
 		} else {
-			type_info().destruct((byte*)&memory_);
-			type_index_ = I;
-			type_info().move_or_copy_construct((byte*)&memory_, (byte*)&value);
+			destruct();
+			construct_move(move(value));
 		}
 	}
 	
 	template <typename... Types>
 	template <typename T>
 	void Either<Types...>::assign_copy(const T& value) {
-		static const size_t I = IndexOfType<T, Types...>::Value;
 		if (is_a<T>()) {
-			type_info().copy_assign((byte*)&memory_, (const byte*)&value);
+			*reinterpret_cast<T*>(memory()) = value;
 		} else {
-			type_info().destruct((byte*)&memory_);
-			type_index_ = I;
-			type_info().copy_construct((byte*)&memory_, (const byte*)&value);
+			destruct();
+			construct_copy(value);
 		}
+	}
+	
+	template <typename... Types>
+	template <typename T>
+	void Either<Types...>::construct_move(T&& value) {
+		static_assert(IndexOfType<T, Types...>::Value < sizeof...(Types), "Invalid type for this Either.");
+		ASSERT(type_index_ == UINT8_MAX); // Already constructed!
+		type_index_ = IndexOfType<T, Types...>::Value;
+		new(memory()) T(move(value));
+	}
+	
+	template <typename... Types>
+	template <typename T>
+	void Either<Types...>::construct_copy(const T& value) {
+		static_assert(IndexOfType<T, Types...>::Value < sizeof...(Types), "Invalid type for this Either.");
+		ASSERT(type_index_ == UINT8_MAX); // Already constructed!
+		type_index_ = IndexOfType<T, Types...>::Value;
+		new(memory()) T(value);
 	}
 	
 	namespace detail {
---
+++ type_traits.hpp
@@ -0,0 +1,109 @@
+//
+//  type_traits.hpp
+//  falling
+//
+//  Created by Simon Ask Ulsnes on 04/05/13.
+//  Copyright (c) 2013 Simon Ask Consulting. All rights reserved.
+//
+
+#ifndef falling_type_traits_hpp
+#define falling_type_traits_hpp
+
+#include <type_traits>
+
+namespace falling {
+	// TODO: Move to a math-related header?
+	template <typename A, typename B>
+	constexpr auto max(A a, B b) -> typename std::common_type<A, B>::type {
+		return a > b ? a : b;
+	}
+
+	// === MaxSize ===
+	template <typename... Types> struct MaxSize;
+	template <> struct MaxSize<> {
+		static const size_t Value = 0;
+	};
+	template <typename T, typename... Rest> struct MaxSize<T, Rest...> {
+		static const size_t Value = max(sizeof(T), MaxSize<Rest...>::Value);
+	};
+	
+	// === MaxAlignment ===
+	template <typename... Types> struct MaxAlignment;
+	template <> struct MaxAlignment<> {
+		static const size_t Value = 0;
+	};
+	template <typename T, typename... Rest> struct MaxAlignment<T, Rest...> {
+		static const size_t Value = max(alignof(T), MaxAlignment<Rest...>::Value);
+	};
+	
+	// === IndexOfType ===
+	template <typename T, size_t I, typename... Types> struct IndexOfTypeImpl;
+	template <typename T, size_t I, typename Current, typename... Rest> struct IndexOfTypeImpl<T, I, Current, Rest...> {
+		static const size_t Value = std::is_same<T, Current>::value ? I : IndexOfTypeImpl<T, I+1, Rest...>::Value;
+	};
+	template <typename T, size_t I> struct IndexOfTypeImpl<T, I> {
+		static const size_t Value = SIZE_MAX;
+	};
+	template <typename T, typename... Types> struct IndexOfType {
+		static const size_t Value = IndexOfTypeImpl<T, 0, Types...>::Value;
+	};
+	
+	// === AlignedUnion ===
+	// TODO: Use std::aligned_union once libc++ catches up with C++11.
+	template <typename... Types>
+	struct AlignedUnion {
+		using Type = typename std::aligned_storage<MaxSize<Types...>::Value, MaxAlignment<Types...>::Value>::type;
+	};
+	
+	// AllTypesSatisfyTrait_standard
+	template <template <typename T> class Trait, typename... Types> struct AllTypesSatifyTrait_standard;
+	template <template <typename T> class Trait, typename First, typename... Rest> struct AllTypesSatifyTrait_standard<Trait, First, Rest...> {
+		static const bool Value = Trait<First>::value && AllTypesSatifyTrait_standard<Trait, Rest...>::Value;
+	};
+	template <template <typename T> class Trait> struct AllTypesSatifyTrait_standard<Trait> {
+		static const bool Value = true;
+	};
+	// AnyTypesSatisfyTrait_standard
+	template <template <typename T> class Trait, typename... Types> struct AnyTypesSatifyTrait_standard;
+	template <template <typename T> class Trait, typename First, typename... Rest> struct AnyTypesSatifyTrait_standard<Trait, First, Rest...> {
+		static const bool Value = Trait<First>::value || AllTypesSatifyTrait_standard<Trait, Rest...>::Value;
+	};
+	template <template <typename T> class Trait> struct AnyTypesSatifyTrait_standard<Trait> {
+		static const bool Value = false;
+	};
+	// AllTypesSatisfyTrait_falling
+	template <template <typename T> class Trait, typename... Types> struct AllTypesSatifyTrait_falling;
+	template <template <typename T> class Trait, typename First, typename... Rest> struct AllTypesSatifyTrait_falling<Trait, First, Rest...> {
+		static const bool Value = Trait<First>::Value && AllTypesSatifyTrait_falling<Trait, Rest...>::Value;
+	};
+	template <template <typename T> class Trait> struct AllTypesSatifyTrait_falling<Trait> {
+		static const bool Value = true;
+	};
+	// AnyTypesSatisfyTrait_falling
+	template <template <typename T> class Trait, typename... Types> struct AnyTypesSatifyTrait_falling;
+	template <template <typename T> class Trait, typename First, typename... Rest> struct AnyTypesSatifyTrait_falling<Trait, First, Rest...> {
+		static const bool Value = Trait<First>::Value || AllTypesSatifyTrait_falling<Trait, Rest...>::Value;
+	};
+	template <template <typename T> class Trait> struct AnyTypesSatifyTrait_falling<Trait> {
+		static const bool Value = false;
+	};
+	
+	// AreAllCopyConstructible
+	template <typename... Types> struct AreAllCopyConstructible {
+		static const bool Value = AllTypesSatifyTrait_standard<std::is_copy_constructible, Types...>::Value;
+	};
+	// AreAllCopyAssignable
+	template <typename... Types> struct AreallCopyAssignable {
+		static const bool Value = AllTypesSatifyTrait_standard<std::is_copy_assignable, Types...>::Value;
+	};
+	// AreAllMoveConstructible
+	template <typename... Types> struct AreAllMoveConstructible {
+		static const bool Value = AllTypesSatifyTrait_standard<std::is_move_constructible, Types...>::Value;
+	};
+	// AreAllMoveAssignable
+	template <typename... Types> struct AreallMoveAssignable {
+		static const bool Value = AllTypesSatifyTrait_standard<std::is_move_assignable, Types...>::Value;
+	};
+}
+
+#endif
