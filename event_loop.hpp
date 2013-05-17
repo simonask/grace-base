9ebf48189ee1f2258091aa2f744fbca2d6f8c9dd
Simon Ask Ulsnes <simon@ulsnes.dk>
2013-05-17 20:31:03 +0100

    * Moved event types to a separate header. * Added API to record and replay events. * Added Key enum with all supported keys.

--- event_loop.hpp
+++ event_loop.hpp
@@ -15,63 +15,16 @@
 #include "base/string.hpp"
 #include "event/event_handle.hpp"
 #include "base/function.hpp"
+#include "event/events.hpp"
 
 namespace falling {
 	using FileSystemDescriptor = int;
-	
-	enum class FileSystemEvent : uint8 {
-		Read       = 1 << 0, // Callback will be called when the fd is ready to be read from.
-		Write      = 1 << 1, // Callback will be called when the fd is ready to be written to.
-		Persistent = 1 << 2, // Event will be re-added to the event loop when triggered.
-		Timeout    = 1 << 3, // Callback will be called when the fd timed out.
-	};
-	ENUM_IS_FLAGS(FileSystemEvent);
-	
-	enum class InputEventType : uint32 {
-		// Keyboard events
-		KeyUp   = 1 << 0,
-		KeyDown = 1 << 1,
-		KeyText = 1 << 2,
-		KeyEventMask = 0x7,
-		
-		// Mouse events  (data[0] == button number)
-		MouseDown = 1 << 8,
-		MouseUp   = 1 << 9,
-		MouseMove = 1 << 10,
-		MouseEventMask = 0x700,
-	};
-	ENUM_IS_FLAGS(InputEventType);
-	
-	enum class KeyboardModifier : uint8 {
-		Shift = 1 << 0,
-		CtrlOrCmd = 1 << 1,
-		WinOrCtrl = 1 << 2,
-		Alt = 1 << 3,
-	};
-	ENUM_IS_FLAGS(KeyboardModifier);
-	
-	struct InputEvent {
-		InputEventType type;
-		byte data[4]; // enough to hold all UTF-8 chars?
-		
-		bool is_key_event() const { return ((uint32)type & (uint32)InputEventType::KeyEventMask) != 0; }
-		bool is_mouse_event() const { return ((uint32)type & (uint32)InputEventType::MouseEventMask) != 0; }
-		bool has_text() const { return is_key_event() && data[0] != 0; }
-		bool is_text() const { return type == InputEventType::KeyText; }
-		bool is_modifier() const { return is_key_event() && data[0] == 0; }
-		StringRef text() const { return StringRef((const char*)data, text_length()); }
-		
-		size_t text_length() const {
-			// I bet the compiler can optimize this. Right...?!
-			return ::strnlen((const char*)data, 4);
-		}
-	};
 	
 	using CallbackID = uintptr_t;
 
 	struct IEventLoop {
 		using FileSystemCallback = Function<void(FileSystemDescriptor fd, FileSystemEvent event)>;
-		using InputEventCallback = Function<bool(const InputEvent&)>; // return: handled?
+		using InputEventCallback = Function<EventResponse(const InputEvent&)>; // return: handled?
 	
 		// Call-later API
 		virtual IEventHandle* schedule(IAllocator&, Function<void()>, SystemTimeDelta delay) = 0;
@@ -80,6 +33,10 @@ namespace falling {
 		// Input Event API
 		virtual IEventHandle* listen_for_input_event(IAllocator&, uint32 input_event_mask, InputEventCallback callback) = 0;
 		virtual void push_input_event(InputEvent event, bool handle_immediately = true) = 0;
+		
+		// Input Event Recording API
+		virtual void record_input_events(StringRef output_file_path) = 0;
+		virtual void replay_recorded_input_events(StringRef recorded_events_file_path, bool authentic_time) = 0;
 		
 		// Async File I/O API
 		virtual IEventHandle* watch_descriptor(IAllocator&, FileSystemDescriptor fd, uint8 event_mask, FileSystemCallback callback, SystemTimeDelta timeout = SystemTimeDelta::forever()) = 0;
---
+++ events.hpp
@@ -0,0 +1,88 @@
+//
+//  events.hpp
+//  falling
+//
+//  Created by Simon Ask Ulsnes on 14/05/13.
+//  Copyright (c) 2013 Simon Ask Consulting. All rights reserved.
+//
+
+#ifndef falling_events_hpp
+#define falling_events_hpp
+
+#include "event/keys.hpp"
+#include "base/vector.hpp"
+
+namespace falling {
+	enum class FileSystemEvent : uint8 {
+		Read       = 1 << 0, // Callback will be called when the fd is ready to be read from.
+		Write      = 1 << 1, // Callback will be called when the fd is ready to be written to.
+		Persistent = 1 << 2, // Event will be re-added to the event loop when triggered.
+		Timeout    = 1 << 3, // Callback will be called when the fd timed out.
+	};
+	ENUM_IS_FLAGS(FileSystemEvent);
+	
+	enum class InputEventType {
+		// Keyboard events
+		KeyUp   = 1 << 0,
+		KeyDown = 1 << 1,
+		KeyTextInput = 1 << 2,
+		KeyTextEdit  = 1 << 3,
+		KeyEventMask = 0xf,
+		
+		// Mouse events
+		MouseDown  = 1 << 8,
+		MouseUp    = 1 << 9,
+		MouseMove  = 1 << 10,
+		MouseWheel = 1 << 11,
+		MouseEventMask = 0xf00,
+		
+		AllEventsMask = 0xffff
+	};
+	ENUM_IS_FLAGS(InputEventType);
+	
+	
+	struct InputEvent {
+		InputEventType type;
+		union {
+			struct {
+				char character[4]; // 1 utf8 character
+				
+				// for editing events:
+				uint16 offset;
+				uint16 length;
+			} text;
+			struct {
+				Key key;
+				uint32 modifiers;
+				bool is_repeat;
+			} key;
+			struct {
+				uint8 button;
+			} mouse;
+			struct {
+				int32 x;
+				int32 y;
+			} wheel;
+			struct {
+				struct {int32 x; int32 y;} position;
+				struct {int32 x; int32 y;} movement;
+			} move;
+		};
+		
+		
+		bool is_key_event() const { return ((uint32)type & (uint32)InputEventType::KeyEventMask) != 0; }
+		bool is_mouse_event() const { return ((uint32)type & (uint32)InputEventType::MouseEventMask) != 0; }
+		StringRef str() const { return StringRef(text.character, str_length()); }
+		size_t str_length() const {
+			// I bet the compiler can optimize this. Right...?!
+			return ::strnlen(text.character, 4);
+		}
+	};
+	
+	enum class EventResponse {
+		PassOn,
+		Handled,
+	};
+}
+
+#endif
