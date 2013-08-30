b5148a8df691ba3a6a4b6de565c0e8eabf3a62c8
Simon Ask Ulsnes <simon@ulsnes.dk>
2013-08-30 10:39:33 +0200

    ProcessStream

---
+++ io/process_stream.cpp
@@ -0,0 +1,102 @@
+#include "io/process_stream.hpp"
+#include "base/stack_array.hpp"
+
+namespace grace {
+		ProcessStream ProcessStream::open(StringRef cmd, ProcessStreamMode mode) {
+			const char* m;
+			switch (mode) {
+				case ProcessStreamMode::Read: m = "r"; break;
+				case ProcessStreamMode::Write: m = "w"; break;
+				case ProcessStreamMode::ReadWrite: m = "r+"; break;
+			}
+			COPY_STRING_REF_TO_CSTR_BUFFER(cmd_buffer, cmd);
+			FILE* fp = ::popen(cmd_buffer.data(), m);
+			ProcessStream ps(fp, mode);
+			if (fp == nullptr) {
+				ps.exitcode_ = 127;
+			}
+			return move(ps);
+		}
+
+		ProcessStream::~ProcessStream() {
+			close();
+		}
+
+		ProcessStream::ProcessStream(ProcessStream&& other) : fp_(other.fp_), mode_(other.mode_), exitcode_(other.exitcode_) {
+			other.fp_ = nullptr;
+			other.exitcode_ = -1;
+		}
+
+		ProcessStream& ProcessStream::operator=(ProcessStream&& other) {
+			close();
+			fp_ = other.fp_;
+			mode_ = other.mode_;
+			exitcode_ = other.exitcode_;
+			other.fp_ = nullptr;
+			other.exitcode_ = -1;
+			return *this;
+		}
+
+		void ProcessStream::close() {
+			if (fp_) {
+				exitcode_ = ::pclose((FILE*)fp_);
+				fp_ = nullptr;
+			}
+		}
+
+		int ProcessStream::wait() {
+			close();
+			return exitcode_;
+		}
+
+		// InputStream interface
+		bool ProcessStream::is_readable() const {
+			return mode_ == ProcessStreamMode::Read || mode_ == ProcessStreamMode::ReadWrite;
+		}
+
+		size_t ProcessStream::read(byte* buffer, size_t max) {
+			ASSERT(is_readable());
+			if (fp_) {
+				return ::fread((void*)buffer, max, 1, (FILE*)fp_);
+			}
+			return 0;
+		}
+
+		size_t ProcessStream::tell_read() const {
+			if (fp_) {
+				return ::ftell((FILE*)fp_);
+			}
+			return 0;
+		}
+
+		bool ProcessStream::seek_read(size_t position) {
+			return false;
+		}
+
+		// OutputStream interface
+		bool ProcessStream::is_writable() const {
+			return mode_ == ProcessStreamMode::Write || mode_ == ProcessStreamMode::ReadWrite;
+		}
+
+		size_t ProcessStream::write(const byte* buffer, size_t max) {
+			ASSERT(is_writable());
+			if (fp_) {
+				return ::fwrite((const void*)buffer, max, 1, (FILE*)fp_);
+			}
+			return 0;
+		}
+
+		size_t ProcessStream::tell_write() const {
+			return 0;
+		}
+
+		bool ProcessStream::seek_write(size_t position) {
+			return false;
+		}
+
+		void ProcessStream::flush() {
+			if (fp_) {
+				::fflush((FILE*)fp_);
+			}
+		}
+}
---
+++ io/process_stream.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#ifndef GRACE_BASE_PROCESS_STREAM_HPP
+#define GRACE_BASE_PROCESS_STREAM_HPP
+
+#include "io/input_stream.hpp"
+#include "io/output_stream.hpp"
+#include "base/string.hpp"
+
+namespace grace {
+	enum class ProcessStreamMode : uint8 {
+		Read = 1,
+		Write = 2,
+		ReadWrite = 3,
+	};
+
+
+
+	class ProcessStream : public InputStream, public OutputStream {
+	public:
+		static ProcessStream open(StringRef cmd, ProcessStreamMode mode = ProcessStreamMode::ReadWrite);
+
+		ProcessStream(ProcessStream&& other);
+		ProcessStream& operator=(ProcessStream&& other);
+		virtual ~ProcessStream();
+
+		int wait();
+		void close();
+
+		// InputStream interface
+		bool is_readable() const final;
+		size_t read(byte* buffer, size_t max) final;
+		size_t tell_read() const final;
+		bool seek_read(size_t position) final;
+		bool has_length() const final { return false; }
+		size_t length() const final { return SIZE_MAX; }
+
+		// OutputStream interface
+		bool is_writable() const final;
+		size_t write(const byte* buffer, size_t max) final;
+		size_t tell_write() const final;
+		bool seek_write(size_t position) final;
+		void flush() final;
+	private:
+		ProcessStream(void* os_fp, ProcessStreamMode mode) : fp_(os_fp), mode_(mode) {}
+		void* fp_;
+		ProcessStreamMode mode_;
+		int exitcode_ = -1;
+	};
+}
+
+#endif
